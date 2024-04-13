2f3054b7d7b9040a441e5966630c02441f137d9a
maulik kanani <kananimaulik26@gmail.com>
2024-04-13 12:14:45 +0530

    new changes

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
-const express = require('express');
-const dotEnv = require('dotenv');
-const cors = require('cors');
-dotEnv.config();
-
-const router = require('./routes');
-
+const express = require("express");
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use("/api", router);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
